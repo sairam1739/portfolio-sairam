2c79dc09c1891f14cdfc4c5acc82ba1698773ad9
sairam1739 <117503218+sairam1739@users.noreply.github.com>
2025-01-24 10:58:42 +0530

    Update Home.jsx

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import pdf from "../pdf/sairamresumeutd.pdf";
+import pdf from "../pdf/sairam.pdf";
 import mypropic from "./pp/profilepic.jpg";
 import mypropic2 from "./pp/file2.png";
 import Typed from "typed.js";
